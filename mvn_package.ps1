c6997aed3f7a3f902119ab382a8aba4caf8bf3f3
Arouna GANOU <arouna.ganou@gmail.com>
2023-06-27 06:54:13 +0200

    [GLS][OBA] Add powershell file to generate jar with external dependencies : Add build without test

--- mvn_package.ps1
+++ mvn_package.ps1
@@ -1,1 +1,4 @@
-mvn `-Dmaven.test.skip=true install
+$workspace = "$PSScriptRoot";
+$env:path = "${env:path};C:\Program Files\Java\apache-maven-3.9.3\bin"
+cd $workspace;
+mvn `-Dmaven.test.skip=true package;
